f64803d17cb37f80f7c2ddf0525d7060e38bf7aa
robmachado <linux.rlm@gmail.com>
2019-03-11 14:46:31 -0300

    [UPD] ajuste nomenclatura do processo

--- src/Common/Tools.php
+++ src/Common/Tools.php
@@ -97,7 +97,11 @@ class Tools
             "Content-length: $msgSize"
         ];
         $response = (string) $this->soap->send(
-            $operation, $url, $action, $request, $parameters
+            $operation,
+            $url,
+            $action,
+            $request,
+            $parameters
         );
         return $response;
     }
@@ -118,7 +122,6 @@ class Tools
         $content = $dom->saveXML($dom->documentElement);
         return $content;
         */
-        
     }
 
     /**
@@ -142,58 +145,4 @@ class Tools
 
         return $env;
     }
-
-    protected function get_decorated_diff($old, $new)
-    {
-        $from_start = strspn($old ^ $new, "\0");
-        $from_end = strspn(strrev($old) ^ strrev($new), "\0");
-
-        $old_end = strlen($old) - $from_end;
-        $new_end = strlen($new) - $from_end;
-
-        $start = substr($new, 0, $from_start);
-        $end = substr($new, $new_end);
-        $new_diff = substr($new, $from_start, $new_end - $from_start);
-        $old_diff = substr($old, $from_start, $old_end - $from_start);
-
-        $new = "$start<ins style='background-color:#ccffcc'>$new_diff</ins>$end";
-        $old = "$start<del style='background-color:#ffcccc'>$old_diff</del>$end";
-        return array("old" => $old, "new" => $new);
-    }
-
-    protected function diff($old, $new)
-    {
-        $maxlen = 0;
-        foreach ($old as $oindex => $ovalue) {
-            $nkeys = array_keys($new, $ovalue);
-            foreach ($nkeys as $nindex) {
-                $matrix[$oindex][$nindex] = isset($matrix[$oindex - 1][$nindex - 1]) ?
-                    $matrix[$oindex - 1][$nindex - 1] + 1 : 1;
-                if ($matrix[$oindex][$nindex] > $maxlen) {
-                    $maxlen = $matrix[$oindex][$nindex];
-                    $omax = $oindex + 1 - $maxlen;
-                    $nmax = $nindex + 1 - $maxlen;
-                }
-            }
-        }
-        if ($maxlen == 0)
-            return array(array('d' => $old, 'i' => $new));
-        return array_merge(
-            $this->diff(array_slice($old, 0, $omax), array_slice($new, 0, $nmax)), array_slice($new, $nmax, $maxlen), $this->diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen)));
-    }
-
-    protected function htmlDiff($old, $new)
-    {
-        $ret = "";
-        $diff = $this->diff(explode(' ', $old), explode(' ', $new));
-        foreach ($diff as $k) {
-            if (is_array($k))
-                $ret .= (!empty($k['d']) ? "<del>" . implode(' ', $k['d']) . "</del> " : '') .
-                    (!empty($k['i']) ? "<ins>" . implode(' ', $k['i']) . "</ins> " : '');
-            else
-                $ret .= $k . ' ';
-        }
-        return $ret;
-    }
-
 }
